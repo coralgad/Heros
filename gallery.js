9d985693c89407d7f7bf5aafe76f0885df2910c1
MayaMeir <themayameir@gmail.com>
2021-01-26 16:42:28 +0200

    can add multiple pics, no real fight yet

--- gallery.js
+++ gallery.js
@@ -10,12 +10,18 @@ let newHeroString = localStorage.getItem('Hero');
 
 newHero = JSON.parse(newHeroString);
 
+let newHerosString = localStorage.getItem('Heros');
 
-let heros = [new Hero('Spiderman', 'Overall Coolness and spidey powers!', 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'),
-new Hero('Wolverine', 'Badassery. Thats it.', 'https://fastly.syfy.com/sites/syfy/files/styles/1200x680/public/2020/02/stl149580.jpg?offset-x=0&offset-y=0'),
-new Hero('Deadpool', 'Cant die and great sense of humor.', 'https://miro.medium.com/max/3840/1*0ubYRV_WNR9iYrzUAVINHw.jpeg')];
+let heros = JSON.parse(newHerosString);
 
-if (newHero != null){
+if (heros == null) {
+
+    heros = [new Hero('Spiderman', 'Overall Coolness and spidey powers!', 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'),
+    new Hero('Wolverine', 'Badassery. Thats it.', 'https://fastly.syfy.com/sites/syfy/files/styles/1200x680/public/2020/02/stl149580.jpg?offset-x=0&offset-y=0'),
+    new Hero('Deadpool', 'Cant die and great sense of humor.', 'https://miro.medium.com/max/3840/1*0ubYRV_WNR9iYrzUAVINHw.jpeg')];
+}
+
+if (newHero != null) {
     heros.push(newHero);
 }
 
@@ -43,3 +49,5 @@ function addToLocal() {
     let herosString = JSON.stringify(heros); //now its string
     localStorage.setItem('Heros', herosString);
 }
+
+addToLocal();
