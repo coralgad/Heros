4f61bcf6567713a2eaaa28f4d0812bcbf9440ece
MayaMeir <themayameir@gmail.com>
2021-01-26 13:48:12 +0200

    added gallery, not finished

---
+++ gallery.js
@@ -0,0 +1,33 @@
+class Hero {
+    constructor(name, power, img) {
+        this.name = name;
+        this.power = power;
+        this.img = img;
+    }
+}
+
+let heros = [new Hero('Spiderman', 'Overall Coolness and spidey powers!', 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'),
+new Hero('Wolverine', 'Badassery. Thats it.', 'https://fastly.syfy.com/sites/syfy/files/styles/1200x680/public/2020/02/stl149580.jpg?offset-x=0&offset-y=0'),
+new Hero('Deadpool', 'Cant die and great sense of humor.', 'https://miro.medium.com/max/3840/1*0ubYRV_WNR9iYrzUAVINHw.jpeg')];
+
+
+function showHeros() {
+
+    let gallery = document.querySelector("#gallery");
+    let i=0;
+    for (const hero of heros) {
+        
+        gallery.innerHTML += `<div class="col-4"><div class="card">
+            <img src="${hero.img}" class="card-img-top" alt="${hero.name}">
+            <div class="card-body">
+                <h5 class="card-title">${hero.name}</h5>
+                <p class="card-text">${hero.power}</p>
+            </div>
+        </div>
+    </div>
+`
+}
+}
+    
+showHeros();
+showHeros();
